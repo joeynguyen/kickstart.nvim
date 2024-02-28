23a15aa1dfe76a56e605a84a51a0cc2a280530e2
Joey Nguyen <776016+joeynguyen@users.noreply.github.com>
2024-02-28 01:51:59 -0700

    fix more formatting

--- lua/custom/plugins/telescope.lua
+++ lua/custom/plugins/telescope.lua
@@ -2,9 +2,9 @@
 return { { "nvim-telescope/telescope-ui-select.nvim" }, {
     'nvim-telescope/telescope.nvim',
     branch = '0.1.x',
-    dependencies = { 'nvim-lua/plenary.nvim', -- Fuzzy Finder Algorithm which requires local dependencies to be built.
-        -- Only load if `make` is available. Make sure you have the system
-        -- requirements installed.
+    dependencies = {
+        'nvim-lua/plenary.nvim', -- Fuzzy Finder Algorithm which requires local dependencies to be built.
+        -- Only load if `make` is available. Make sure you have the system requirements installed.
         {
             'nvim-telescope/telescope-fzf-native.nvim',
             -- NOTE: If you are having trouble with this installation,
