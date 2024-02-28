0cf0ee571a5218029a5c4ca12949f6b24dd69594
Joey Nguyen <776016+joeynguyen@users.noreply.github.com>
2024-02-28 01:43:51 -0700

    separate lualine into its own file

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -175,18 +175,6 @@ return {                                       -- NOTE: First, some plugins that
         require('onedark').load()
     end
 }, {
-    -- Set lualine as statusline
-    'nvim-lualine/lualine.nvim',
-    -- See `:help lualine.txt`
-    opts = {
-        options = {
-            icons_enabled = false,
-            theme = 'auto',
-            component_separators = '|',
-            section_separators = ''
-        }
-    }
-}, {
     -- Add indentation guides even on blank lines
     'lukas-reineke/indent-blankline.nvim',
     -- Enable `lukas-reineke/indent-blankline.nvim`
