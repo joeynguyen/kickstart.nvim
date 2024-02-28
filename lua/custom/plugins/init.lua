d96054baca9243bedf982aa9edf6a19f0ae09c87
Joey Nguyen <776016+joeynguyen@users.noreply.github.com>
2024-02-28 01:32:23 -0700

    add neo-tree

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -203,4 +203,12 @@ return {                                       -- NOTE: First, some plugins that
     --       Uncomment any of the lines below to enable them.
     -- require 'kickstart.plugins.autoformat',
     -- require 'kickstart.plugins.debug',
-}
+    {
+        "nvim-neo-tree/neo-tree.nvim",
+        branch = "v3.x",
+        dependencies = { "nvim-lua/plenary.nvim", "nvim-tree/nvim-web-devicons", "MunifTanjim/nui.nvim" },
+        config = function()
+            vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal left<CR>", {})
+            vim.keymap.set("n", "<leader>bf", ":Neotree buffers reveal float<CR>", {})
+        end
+    } }
