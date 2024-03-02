f4f1af25448bc6475ab5961c8ac41ff286a9408d
Joey Nguyen <776016+joeynguyen@users.noreply.github.com>
2024-03-02 04:54:39 -0600

    change NeoTree shortcut to toggle instead of reveal

--- lua/custom/plugins/neo-tree.lua
+++ lua/custom/plugins/neo-tree.lua
@@ -1,6 +1,6 @@
 return {
     "nvim-neo-tree/neo-tree.nvim",
-    version = "*",
+    version = "v3.x",
     dependencies = {
         "nvim-lua/plenary.nvim",
         "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
@@ -8,7 +8,7 @@ return {
     },
     config = function()
         require('neo-tree').setup {}
-        vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal left<CR>", {})
+        vim.keymap.set("n", "<C-n>", ":Neotree filesystem toggle left<CR>", {})
         vim.keymap.set("n", "<leader>bf", ":Neotree buffers reveal float<CR>", {})
     end,
 }
