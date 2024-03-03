2458abc32ecd4982ebfba8a643bcc29aad72a20b
Joey Nguyen <776016+joeynguyen@users.noreply.github.com>
2024-03-03 17:36:17 -0600

    format neo-tree file

--- lua/custom/plugins/neo-tree.lua
+++ lua/custom/plugins/neo-tree.lua
@@ -1,28 +1,28 @@
 return {
-    'nvim-neo-tree/neo-tree.nvim',
-    version = 'v3.x',
-    dependencies = {
-        'nvim-lua/plenary.nvim',
-        'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
-        'MunifTanjim/nui.nvim',
-    },
-    config = function()
-        require('neo-tree').setup {
-            window = {
-                mappings = {
-                    ['Z'] = 'expand_all_nodes',
-                },
-            },
-            filesystem = {
-                filtered_items = {
-                    hide_dotfiles = false,
-                },
-                follow_current_file = {
-                    enabled = true, -- This will find and focus the file in the active buffer every time
-                },
-            },
-        }
-        vim.keymap.set('n', '<C-n>', ':Neotree filesystem toggle left<CR>', {})
-        vim.keymap.set('n', '<C-m>', ':Neotree buffers reveal float<CR>', {})
-    end,
+  'nvim-neo-tree/neo-tree.nvim',
+  version = 'v3.x',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
+    'MunifTanjim/nui.nvim',
+  },
+  config = function()
+    require('neo-tree').setup {
+      window = {
+        mappings = {
+          ['Z'] = 'expand_all_nodes',
+        },
+      },
+      filesystem = {
+        filtered_items = {
+          hide_dotfiles = false,
+        },
+        follow_current_file = {
+          enabled = true, -- This will find and focus the file in the active buffer every time
+        },
+      },
+    }
+    vim.keymap.set('n', '<C-n>', ':Neotree filesystem toggle left<CR>', {})
+    vim.keymap.set('n', '<C-m>', ':Neotree buffers reveal float<CR>', {})
+  end,
 }
