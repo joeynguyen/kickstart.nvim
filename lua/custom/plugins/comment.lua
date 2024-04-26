9bb2c29daeebc7c3312dcea2eaaaf5f1f33c4c07
Joey Nguyen <joey.nguyen@slingshotaerospace.com>
2024-04-26 13:59:01 -0500

    use 'gc' instead of 'gcc' to comment current line

--- lua/custom/plugins/comment.lua
+++ lua/custom/plugins/comment.lua
@@ -1,5 +1,10 @@
 return {
-    -- "gc" to comment visual regions/lines
-    'numToStr/Comment.nvim',
-    opts = {},
+  -- "gc" to comment visual regions/lines
+  'numToStr/Comment.nvim',
+  opts = {
+    toggler = {
+      -- Line-comment toggle keymap
+      line = 'gc',
+    },
+  },
 }
