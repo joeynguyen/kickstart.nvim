f11aa9ac470773320c43a034b1b5f797c5a56b2f
Joey Nguyen <joey.nguyen@slingshotaerospace.com>
2024-04-26 13:59:41 -0500

    use Ctrl-f to search for text in files

--- lua/custom/plugins/telescope.lua
+++ lua/custom/plugins/telescope.lua
@@ -79,6 +79,7 @@ return {
 
       -- See `:help telescope.builtin`
       vim.keymap.set('n', '<C-p>', builtin.find_files, {})
+      vim.keymap.set('n', '<C-f>', builtin.live_grep, {})
       vim.keymap.set('n', '<leader>fg', builtin.live_grep, {
         desc = '[F]ile [G]rep',
       })
