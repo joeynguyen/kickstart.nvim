ff66ef6b1b7e104504abb3e18eec3dc914e0a8d7
Joey Nguyen <776016+joeynguyen@users.noreply.github.com>
2024-05-27 04:52:40 -0500

    add flash plugin

--- lua/custom/plugins/noice.lua
+++ lua/custom/plugins/noice.lua
@@ -40,6 +40,7 @@ return {
       routes = {
         -- disable text search confirmation
         { filter = { event = 'msg_show', find = '/' },               skip = true },
+        { filter = { event = 'msg_show', find = 'lines yanked' },    skip = true },
         { filter = { event = 'msg_show', find = 'more line' },       skip = true },
         { filter = { event = 'msg_show', find = 'line less' },       skip = true },
         { filter = { event = 'msg_show', find = 'changes; before' }, skip = true },
--- lua/custom/plugins/telescope.lua
+++ lua/custom/plugins/telescope.lua
@@ -56,8 +56,7 @@ return {
         end
 
         -- Find the Git root directory from the current file's path
-        local git_root = vim.fn.systemlist('git -C ' .. vim.fn.escape(current_dir, ' ') .. ' rev-parse --show-toplevel')
-        [1]
+        local git_root = vim.fn.systemlist('git -C ' .. vim.fn.escape(current_dir, ' ') .. ' rev-parse --show-toplevel')[1]
         if vim.v.shell_error ~= 0 then
           print 'Not a git repository. Searching on current working directory'
           return cwd
@@ -148,6 +147,9 @@ return {
       })
       vim.keymap.set('n', '<leader>sr', builtin.resume, {
         desc = '[S]earch [R]esume',
+      })
+      vim.keymap.set('n', '<leader>ek', builtin.keymaps, {
+        desc = '[K]eymaps',
       })
     end,
   },
