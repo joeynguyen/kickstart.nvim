8ce1883502c098286d087bd5903059ce23edb2ec
Joey Nguyen <joeynguyen@lfe.8shield.net>
2024-06-19 06:36:12 -0500

    replace whitespace plugin because old one had bad default for color and no customization option

--- lua/custom/plugins/whitespace.lua
+++ lua/custom/plugins/whitespace.lua
@@ -1,26 +1,4 @@
 return {
-  'johnfrankmorgan/whitespace.nvim',
-  config = function()
-    require('whitespace-nvim').setup {
-      -- configuration options and their defaults
-
-      -- `highlight` configures which highlight is used to display
-      -- trailing whitespace
-      highlight = 'DiffDelete',
-
-      -- `ignored_filetypes` configures which filetypes to ignore when
-      -- displaying trailing whitespace
-      ignored_filetypes = { 'TelescopePrompt', 'Trouble', 'help' },
-
-      -- `ignore_terminal` configures whether to ignore terminal buffers
-      ignore_terminal = true,
-
-      -- `return_cursor` configures if cursor should return to previous
-      -- position after trimming whitespace
-      return_cursor = true,
-    }
-
-    -- remove trailing whitespace with a keybinding
-    vim.keymap.set('n', '<Leader>w', require('whitespace-nvim').trim, { desc = 'Trim [W]hitespace' })
-  end,
+  'kaplanz/retrail.nvim',
+  opts = {}, -- calls `setup` using provided `opts`
 }
