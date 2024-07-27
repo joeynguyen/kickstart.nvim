1d69a464c412cbda3c999bfaf5f8bde3c8f40976
Joey Nguyen <joeynguyen@lfe.8shield.net>
2024-07-27 17:55:40 -0500

    replaced deprecated api with its replacement

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -111,7 +111,7 @@ vim.api.nvim_create_autocmd({ 'BufReadPost' }, {
   pattern = { '*' },
   callback = function()
     if vim.fn.line '\'"' > 1 and vim.fn.line '\'"' <= vim.fn.line '$' then
-      vim.api.nvim_exec('normal! g\'"', false)
+      vim.api.nvim_exec2('normal! g\'"', {})
     end
   end,
 })
