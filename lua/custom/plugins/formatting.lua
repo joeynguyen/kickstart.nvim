44e8cb49b8ff9ae70d6ec8d20d128097925a7a5b
Joey Nguyen <joeynguyen@lfe.8shield.net>
2024-09-29 13:27:26 -0500

    add React and Typescript formatting

--- lua/custom/plugins/formatting.lua
+++ lua/custom/plugins/formatting.lua
@@ -56,6 +56,10 @@ return { -- Autoformat
       --
       -- You can use 'stop_after_first' to run the first available formatter from the list
       javascript = { 'prettierd', 'prettier', stop_after_first = true },
+      javascriptreact = { 'prettierd', 'prettier', stop_after_first = true },
+      typescript = { 'prettierd', 'prettier', stop_after_first = true },
+      typescriptreact = { 'prettierd', 'prettier', stop_after_first = true },
+
       json = { 'prettierd', 'prettier', stop_after_first = true },
 
       ['markdown'] = { 'prettier', 'markdownlint-cli2', 'markdown-toc' },
