0c37c102c5776b9b8338982c9d03999989ad4055
Joey Nguyen <joeynguyen@lfe.8shield.net>
2024-09-29 12:28:58 -0500

    update Neovide animations

--- lua/neovide.lua
+++ lua/neovide.lua
@@ -9,6 +9,15 @@ if vim.g.neovide then
 
   vim.keymap.set('i', '<D-v>', '<C-r><C-o>+') -- paste (insert)
 
+  -- change animation duration
+  vim.g.neovide_position_animation_length = 0
+  vim.g.neovide_cursor_animation_length = 0.05
+  vim.g.neovide_cursor_trail_size = 0.04
+  vim.g.neovide_cursor_animate_in_insert_mode = false
+  vim.g.neovide_cursor_animate_command_line = false
+  vim.g.neovide_scroll_animation_far_lines = 0
+  vim.g.neovide_scroll_animation_length = 0.00
+
   --[[ others
   -- command mapping
   vim.keymap.set({ 'i', 'n' }, '<D-a>', '<ESC>ggVG')                              -- select all
