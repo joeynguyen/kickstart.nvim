ea023270aaa243c5896430af42c805fb6ac1690a
Joey Nguyen <joey.nguyen@slingshotaerospace.com>
2024-12-03 10:58:38 -0600

    re-enable lazydev

--- lua/custom/plugins/lsp-config.lua
+++ lua/custom/plugins/lsp-config.lua
@@ -54,17 +54,17 @@ return {
 
     -- Allows extra capabilities provided by nvim-cmp
     'hrsh7th/cmp-nvim-lsp',
-    -- {
-    --   'folke/lazydev.nvim',
-    --   ft = 'lua', -- only load on lua files
-    --   opts = {
-    --     library = {
-    --       -- See the configuration section for more details
-    --       -- Load luvit types when the `vim.uv` word is found
-    --       { path = 'luvit-meta/library', words = { 'vim%.uv' } },
-    --     },
-    --   },
-    -- },
+    {
+      'folke/lazydev.nvim',
+      ft = 'lua', -- only load on lua files
+      opts = {
+        library = {
+          -- See the configuration section for more details
+          -- Load luvit types when the `vim.uv` word is found
+          { path = 'luvit-meta/library', words = { 'vim%.uv' } },
+        },
+      },
+    },
     -- this plugin is nice but currently has issues with renaming variables exported and imported
     -- in React/Typescript
     -- {
