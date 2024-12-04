1c1a891dcb90e6fa4b5db4aa7ac80effb4d9385b
Joey Nguyen <joeynguyen@lfe.8shield.net>
2024-12-03 22:46:54 -0600

    use leader-t for TextCase plugin

--- lua/custom/plugins/change-case.lua
+++ lua/custom/plugins/change-case.lua
@@ -2,19 +2,22 @@ return {
   'johmsalas/text-case.nvim',
   dependencies = { 'nvim-telescope/telescope.nvim' },
   config = function()
-    require('textcase').setup {}
+    require('textcase').setup {
+      prefix = "<leader>t"
+    }
     require('telescope').load_extension 'textcase'
     -- `cr` shortcut inspired by https://github.com/gregorias/coerce.nvim
-    vim.api.nvim_set_keymap('n', 'cr', '<cmd>TextCaseOpenTelescope<CR>', { desc = 'TextCase [R]eplace' })
-    vim.api.nvim_set_keymap('v', 'cr', '<cmd>TextCaseOpenTelescope<CR>', { desc = 'TextCase [R]eplace' })
-    vim.api.nvim_set_keymap('n', 'ga.', '<cmd>TextCaseOpenTelescope<CR>', { desc = 'Telescope' })
-    vim.api.nvim_set_keymap('v', 'ga.', '<cmd>TextCaseOpenTelescope<CR>', { desc = 'Telescope' })
-    vim.api.nvim_set_keymap('n', 'gaa', '<cmd>TextCaseOpenTelescopeQuickChange<CR>', { desc = 'Telescope Quick Change' })
-    vim.api.nvim_set_keymap('n', 'gai', '<cmd>TextCaseOpenTelescopeLSPChange<CR>', { desc = 'Telescope LSP Change' })
+    -- vim.api.nvim_set_keymap('n', 'cr', '<cmd>TextCaseOpenTelescope<CR>', { desc = 'TextCase [R]eplace' })
+    -- vim.api.nvim_set_keymap('v', 'cr', '<cmd>TextCaseOpenTelescope<CR>', { desc = 'TextCase [R]eplace' })
+    -- vim.api.nvim_set_keymap('n', 'ga.', '<cmd>TextCaseOpenTelescope<CR>', { desc = 'Telescope' })
+    -- vim.api.nvim_set_keymap('v', 'ga.', '<cmd>TextCaseOpenTelescope<CR>', { desc = 'Telescope' })
+    -- vim.api.nvim_set_keymap('n', 'gaa', '<cmd>TextCaseOpenTelescopeQuickChange<CR>', { desc = 'Telescope Quick Change' })
+    -- vim.api.nvim_set_keymap('n', 'gai', '<cmd>TextCaseOpenTelescopeLSPChange<CR>', { desc = 'Telescope LSP Change' })
   end,
   keys = {
-    'ga', -- Default invocation prefix
-    { 'ga.', '<cmd>TextCaseOpenTelescope<CR>', mode = { 'n', 'x' }, desc = 'Telescope' },
+    -- 'ga', -- Default invocation prefix
+    '<leader>t', -- Default invocation prefix
+    -- { 'ga.', '<cmd>TextCaseOpenTelescope<CR>', mode = { 'n', 'x' }, desc = 'Telescope' },
   },
   cmd = {
     -- NOTE: The Subs command name can be customized via the option "substitude_command_name"
