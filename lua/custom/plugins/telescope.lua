c6c7a835364b62b7e1495f82bf641a0cf731e57b
Joey Nguyen <joey.nguyen@slingshotaerospace.com>
2025-10-28 22:13:33 -0500

    autoformat lua file

--- lua/custom/plugins/telescope.lua
+++ lua/custom/plugins/telescope.lua
@@ -74,12 +74,12 @@ return {
           },
           path_display = {
             -- shorten = 5,
-            "tail",
+            'tail',
             -- `filename_first` doesn't seem to work - https://github.com/nvim-telescope/telescope.nvim/issues/3157
             -- "filename_first",
             filename_first = {
-              reverse_directories = true
-            }
+              reverse_directories = true,
+            },
           },
         },
         extensions = {
@@ -97,17 +97,17 @@ return {
             db_safe_mode = false,
             auto_validate = false,
             recency_values = {
-              { age = 1,      value = 10000 }, -- past 1 mins
-              { age = 5,      value = 7000 },  -- past 5 mins
-              { age = 30,     value = 3000 },  -- past 30 mins
-              { age = 60,     value = 1000 },  -- past 1 hour
-              { age = 120,    value = 500 },   -- past 2 hours
-              { age = 240,    value = 100 },   -- past 4 hours
-              { age = 1440,   value = 80 },    -- past day
-              { age = 4320,   value = 60 },    -- past 3 days
-              { age = 10080,  value = 40 },    -- past week
-              { age = 43200,  value = 20 },    -- past month
-              { age = 129600, value = 10 },    -- past 90 days
+              { age = 1, value = 10000 }, -- past 1 mins
+              { age = 5, value = 7000 }, -- past 5 mins
+              { age = 30, value = 3000 }, -- past 30 mins
+              { age = 60, value = 1000 }, -- past 1 hour
+              { age = 120, value = 500 }, -- past 2 hours
+              { age = 240, value = 100 }, -- past 4 hours
+              { age = 1440, value = 80 }, -- past day
+              { age = 4320, value = 60 }, -- past 3 days
+              { age = 10080, value = 40 }, -- past week
+              { age = 43200, value = 20 }, -- past month
+              { age = 129600, value = 10 }, -- past 90 days
             },
             -- ignore_patterns = { '*.git' },
           },
@@ -122,9 +122,8 @@ return {
           },
           oldfiles = {
             initial_mode = 'normal',
-          }
+          },
         },
-
       }
       require('telescope').load_extension 'ui-select'
       require('telescope').load_extension 'frecency'
@@ -149,8 +148,7 @@ return {
         end
 
         -- Find the Git root directory from the current file's path
-        local git_root = vim.fn.systemlist('git -C ' .. vim.fn.escape(current_dir, ' ') .. ' rev-parse --show-toplevel')
-            [1]
+        local git_root = vim.fn.systemlist('git -C ' .. vim.fn.escape(current_dir, ' ') .. ' rev-parse --show-toplevel')[1]
         if vim.v.shell_error ~= 0 then
           print 'Not a git repository. Searching on current working directory'
           return cwd
@@ -176,21 +174,22 @@ return {
       vim.keymap.set('n', '<C-p>', builtin.find_files, { desc = 'Find file by name' })
       vim.keymap.set('n', '<leader>j', function()
         require('telescope').extensions.frecency.frecency {}
-      end, { desc = "Frecency search" })
+      end, { desc = 'Frecency search' })
 
       -- Return/Enter key (also Ctrl+m)
-      vim.keymap.set('n', '<CR>', builtin.buffers, { desc = "View current open buffers", }) -- vim.keymap.set('n', '<leader>b', builtin.buffers, {
+      vim.keymap.set('n', '<CR>', builtin.buffers, { desc = 'View current open buffers' })
+      -- vim.keymap.set('n', '<leader>b', builtin.buffers, { desc = 'View current open buffers' })
 
       -- to exclude multiple folders: additional_args = { '-g', '!node_modules/', '-g', '!dist/', '-g', '!vendor/' }
       vim.keymap.set('n', '<C-f>', function()
-        builtin.live_grep({
+        builtin.live_grep {
           additional_args = function()
             return { '-g', '!patches/' }
-          end
-        })
+          end,
+        }
       end, { desc = 'Search for text in project (excluding patches/)' })
-      vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = '[F]ile [G]rep', })
-      vim.keymap.set('n', '<leader>fr', builtin.oldfiles, { desc = '[F]iles [R]ecently opened', })
+      vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = '[F]ile [G]rep' })
+      vim.keymap.set('n', '<leader>fr', builtin.oldfiles, { desc = '[F]iles [R]ecently opened' })
 
       -- vim.keymap.set('n', '<leader><space>', builtin.buffers, {
       --   desc = '[ ] View current buffers',
