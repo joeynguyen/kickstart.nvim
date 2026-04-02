151e18310cb1ec5d22e5ed7e1391ab6d777bc6ad
Joey Nguyen <joeynguyen@lfe.8shield.net>
2026-04-01 23:52:34 -0500

    add biome formatter/linter

--- lua/custom/plugins/lsp-config.lua
+++ lua/custom/plugins/lsp-config.lua
@@ -221,6 +221,7 @@ return {
     --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
     local servers = {
       prettierd = {},
+      biome = {},
       ['markdownlint-cli2'] = {},
       ['markdown-toc'] = {},
       -- gopls = {},
