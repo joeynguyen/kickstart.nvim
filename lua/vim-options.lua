234bb6dc17b91008d0ba8df164a8521b23eabe68
Joey Nguyen <joeynguyen@lfe.8shield.net>
2026-04-23 00:17:10 -0500

    organize options file and added a few more settings

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -9,85 +9,99 @@ local opt = vim.opt
 --  NOTE: Must happen before plugins are required (otherwise wrong leader will be used)
 g.mapleader = ' '
 g.maplocalleader = ' '
+g.autoformat = true
+g.trouble_lualine = true
+
+-- Behavior settings
+opt.mouse = 'a'    -- Enable mouse mode
+opt.laststatus = 0 -- Disable statusline
+-- Live preview when performing text replace commands, e.g. `:%s/tab/space`
+opt.inccommand = 'split'
+opt.errorbells = false               -- No error bells
+opt.iskeyword:append("-")            -- Treat dash as part of word
+opt.completeopt = 'menuone,noselect' -- Set completeopt to have a better completion experience
+opt.sessionoptions = "blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
+
+-- Visual settings
+opt.termguicolors = true -- Enable 24-bit colors (make sure your terminal supports this)
+
+-- Lines and columns
+opt.cursorline = true      -- Highlight current line
+opt.number = true          -- Make line numbers default
+opt.relativenumber = false -- Relative line numbers
+opt.scrolloff = 4          -- Keep n lines above/below cursor (have context around what you're editing)
+opt.signcolumn = 'yes'     -- Always show sign column
+opt.colorcolumn = '100'    -- Add a vertical line visualizing the column at this number
+opt.wrap = false           -- Don't wrap lines
+
+opt.showmatch = true       -- Highlight matching brackets
+opt.matchtime = 2          -- How long to show matching bracket
+
+-- File handling
+opt.undofile = true  -- Save undo history
+opt.swapfile = false -- Disable swap files
+opt.backup = false   -- Don't create backup files
+opt.autoread = true  -- Auto reload files changed outside vim
 
 -- Performance optimizations
-opt.updatetime = 250
+opt.updatetime = 250 -- Faster completion and git signs
 opt.timeoutlen = 300
+opt.lazyredraw = false
 opt.redrawtime = 10000
 opt.maxmempattern = 20000
+opt.synmaxcol = 300 -- Syntax highlighting limit (prevents freezes on minified files)
 
--- for GUIs like Neovide
--- opt.guifont = 'Rec Mono Semicasual:h20'
--- opt.guifont = 'InconsolataGo Nerd Font:h20'
+-- tabs & indentation
+opt.tabstop = 2        -- Tab width
+opt.softtabstop = 2    -- Soft tab stop
+opt.shiftwidth = 2     -- Indent width
+opt.expandtab = true   -- Use spaces instead of tabs
+opt.smartindent = true -- Smart auto-indenting
+opt.autoindent = true  -- Copy indent from current line
 
--- disable statusline
-opt.laststatus = 0
+-- Enable break indent
+opt.breakindent = true
 
--- add window/split specific tab line
--- %= : float text to right
--- %m : display if file has been modified
--- %f : show file name
--- o.winbar = '%=%m %f'
+-- Search settings
+opt.ignorecase = true -- Case insensitive search
+opt.smartcase = true  -- Case sensitive if uppercase in search
+opt.hlsearch = true   -- Highlight search results
+opt.incsearch = true  -- Show matches as you type
 
--- live preview when performing text replace commands, e.g. `:%s/tab/space`
-opt.inccommand = 'split'
-
--- Set highlight on search
-opt.hlsearch = true
-
--- Set highlight current line
-opt.cursorline = true
-
--- Make line numbers default
-opt.number = true
-
--- Enable mouse mode
-opt.mouse = 'a'
+-- Command-line completion
+opt.wildmenu = true
+opt.wildmode = "longest:full,full"
+opt.wildignore:append({ "*.o", "*.obj", "*.pyc", "*.class", "*.jar" })
 
 -- Sync clipboard between OS and Neovim.
 --  Remove this option if you want your OS clipboard to remain independent.
 --  See `:help 'clipboard'`
 -- o.clipboard = 'unnamedplus'
 
--- tabs & indentation
-opt.tabstop = 2
-opt.softtabstop = 2
-opt.shiftwidth = 2
--- use spaces instead of tabs
-opt.expandtab = true
-opt.smartindent = true
+-- add window/split specific tab line
+-- %= : float text to right
+-- %m : display if file has been modified
+-- %f : show file name
+-- opt.winbar = '%=%m %f'
 
-opt.wrap = false
-opt.lazyredraw = false
+-- for GUIs like Neovide
+-- opt.guifont = 'Rec Mono Semicasual:h20'
+-- opt.guifont = 'InconsolataGo Nerd Font:h20'
 
--- Enable break indent
-opt.breakindent = true
-
--- Save undo history
-opt.undofile = true
-
--- search settings
-opt.swapfile = false
-opt.backup = false
-opt.hlsearch = true
-opt.incsearch = true
-opt.scrolloff = 8
-opt.colorcolumn = '100'
-
--- Case-insensitive searching UNLESS \C or capital in search
-opt.ignorecase = true
-opt.smartcase = true
-
--- Keep signcolumn on by default
-opt.signcolumn = 'yes'
-
--- Set completeopt to have a better completion experience
-opt.completeopt = 'menuone,noselect'
-
--- NOTE: You should make sure your terminal supports this
-opt.termguicolors = true
-
-opt.sessionoptions = "blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
+-- Custom filetype definitions
+vim.filetype.add({
+  extension = {
+    env = "dotenv",
+  },
+  filename = {
+    [".env"] = "dotenv",
+    ["env"] = "dotenv",
+  },
+  pattern = {
+    ["[jt]sconfig.*.json"] = "jsonc",
+    ["%.env%.[%w_.-]+"] = "dotenv",
+  },
+})
 
 require 'utils/visual-sort'
 
