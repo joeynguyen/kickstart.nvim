ced5f595ffb3efc9f372aaa3f17fe6292bee582e
Joey Nguyen <joey.nguyen@slingshotaerospace.com>
2026-04-23 10:40:04 -0500

    plugins: disable unused ones

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -5,9 +5,9 @@
 return { -- NOTE: First, some plugins that don't require any configuration
   -- Git related plugins
   -- 'tpope/vim-fugitive',
-  'tpope/vim-rhubarb', -- Detect tabstop and shiftwidth automatically
-  'tpope/vim-sleuth',
-  'fatih/vim-go',
+  -- 'tpope/vim-rhubarb', -- Detect tabstop and shiftwidth automatically
+  -- 'tpope/vim-sleuth',
+  -- 'fatih/vim-go',
   -- Additional useful plugins
   'tpope/vim-rsi', -- Emacs key bindings in Insert mode
   --[[ {
